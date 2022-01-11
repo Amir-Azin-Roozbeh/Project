d088e2d90c1440bed24125f498e52a8990d25a5f
amirhallaji <a.hallaji.b@gmail.com>
2022-01-11 21:06:26 +0330

    wise robot moves fine

--- controllers/final_controller/final_controller.py
+++ controllers/final_controller/final_controller.py
@@ -5,72 +5,72 @@
 import numpy as np
 from initialization import * 
 from controller import GPS, Compass, DistanceSensor
-#############################################Variables
+import math
+############################################# Variables
 TIME_STEP = 32
 GPS_SAMPLING_PERIOD = 1000
 COMPASS_SAMPLING_PERIOD = 1000
-state = 'move_forward'
 blind_spot_ir = 1000
-############################################Util functions
+x_goal = -1.25
+z_goal = 0.25
+############################################ Util functions
 def calc_distance_to_destination(robot):
     x, z = get_robot_coordinate(robot)
     return math.sqrt(((x_goal-x) ** 2) + ((z_goal-z) ** 2))
 
-# def update_robot_state(ir_value_1):
-    
-#     print(ir_value_1)
-#     if ir_value_1 < blind_spot_ir :
-#         print('in if')
-#         move_robot('stop')
-#     else : 
-#         move_robot('forward')
-
-def get_bearing_in_degrees(north):
+def get_bearing_in_degrees(compass_val):
     # calculate bearing angle in degrees 
-    rad = math.atan2(north[0], north[2])
+    rad = math.atan2(compass_val[0], compass_val[2])
     bearing = (rad - 1.5708) / math.pi * 180.0
     if bearing < 0.0:
         bearing = bearing + 360.0
     return bearing
 
-def move_robot_inertial(x, y, theta, heading):
+def get_robot_heading(compass_val):
+    heading = get_bearing_in_degrees(compass_val)
+    heading = 360-heading
+    heading = heading + 90
+    if heading > 360.0:
+        heading = heading - 360.0
+    return heading
+
+def move_robot(x, y, theta, heading):
     rotation_matrix = np.array([[np.cos(heading), np.sin(heading), 0], [-np.sin(heading), np.cos(heading), 0], [0, 0, 1]])
     temp = np.dot(rotation_matrix, np.array([x, y, theta]))
-    return move_robot(temp[0], temp[1], temp[2])
-
-
-def move_robot(x, y, theta): 
-    # moves = {
-        # 'stop': [0, 0, 0], 
-        # 'rotate_right': [1, 1, 1], 
-        # 'rotate_left': [-1, -1, -1], 
-        # 'forward': [-2, 2, 0], 
-        # 'left': [0, 2, -2], 
-        # 'right': [2, -2, -4], 
-        # 'back': [2, -2, 0]
-    # }
-    
     inverse_matrix = np.array([[-0.33, 0.58, 0.33], [-0.33, -0.58, 0.33], [0.67, 0, 0.33]])
-    speed = np.matmul(inverse_matrix, np.array([x, y, theta]))
+    speed = np.matmul(inverse_matrix, temp)
     update_motor_speed(speed)
-    
+
+def calc_theta_dot(heading, destination_theta) :
+    theta_dot = destination_theta - heading
+    if theta_dot > 180:
+        theta_dot = -(360-theta_dot)
+    elif theta_dot < -180:
+        theta_dot = (360+theta_dot)
+    return theta_dot
+
+def calculate_theta(x, y, compass_val):
+    heading = get_robot_heading(compass_val)
+    theta = math.atan2(y_goal - y, x_goal - x) * 180 / math.pi
+    return calc_theta_dot(heading, theta)
 
 if __name__ == "__main__":
 
     robot = init_robot(time_step=TIME_STEP)
     goal_postition = np.array([0,0])
-    
     # DEFINE STATES HERE!
 
     while robot.step(TIME_STEP) != -1:
 
         gps_values,compass_val,sonar_value,position_value,ir_value = read_sensors_values()
         ir_value_1, ir_value_6, ir_value_3, ir_value_5, ir_value_2, ir_value_4 = ir_value
-        print(get_bearing_in_degrees(compass_val))
        
 
         robot_position = update_robot_state()
-        move_robot_inertial(-10, 0, 0, robot_position[2])
+        if ir_value_1 < blind_spot_ir: 
+            move_robot(0, -10, 0, robot_position[2])
+        else : 
+            move_robot(-10, 0, 0, robot_position[2])
         # update_robot_state(ir_value_1) 
         # move_robot('right') 
         # DEFINE STATE MACHINE HERE!
