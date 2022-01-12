9dce8540e0550d505c9c738608a0ba02d3635979
roozbehsayadi <roozbehsayadi@gmail.com>
2022-01-12 22:17:27 +0330

    moves toward target

--- controllers/final_controller/final_controller.py
+++ controllers/final_controller/final_controller.py
@@ -11,16 +11,17 @@ TIME_STEP = 32
 GPS_SAMPLING_PERIOD = 1000
 COMPASS_SAMPLING_PERIOD = 1000
 blind_spot_ir = 1000
-x_goal = -1.25
-z_goal = 0.25
+X_GOAL = -0.270026
+Y_GOAL = -1.35
+THETA_ACCURACY_THRESH = 10
 ############################################ Util functions
 def calc_distance_to_destination(robot):
     x, z = get_robot_coordinate(robot)
-    return math.sqrt(((x_goal-x) ** 2) + ((z_goal-z) ** 2))
+    return math.sqrt(((X_GOAL-x) ** 2) + ((Y_GOAL-z) ** 2))
 
 def get_bearing_in_degrees(compass_val):
     # calculate bearing angle in degrees 
-    rad = math.atan2(compass_val[0], compass_val[2])
+    rad = math.atan2(compass_val[0], compass_val[1])
     bearing = (rad - 1.5708) / math.pi * 180.0
     if bearing < 0.0:
         bearing = bearing + 360.0
@@ -51,26 +52,52 @@ def calc_theta_dot(heading, destination_theta) :
 
 def calculate_theta(x, y, compass_val):
     heading = get_robot_heading(compass_val)
-    theta = math.atan2(y_goal - y, x_goal - x) * 180 / math.pi
+    theta = math.atan2(Y_GOAL - y, X_GOAL - x) * 180 / math.pi
     return calc_theta_dot(heading, theta)
+
+
+def get_robot_angle_with_target():
+    pass
+ 
+
+class StatesEnum: # Enum
+    MOVE_TOWARD_T = 0
+    FOLLOW_BOUNDARY = 1
+    STOP = 2
 
 if __name__ == "__main__":
 
     robot = init_robot(time_step=TIME_STEP)
     goal_postition = np.array([0,0])
+    state: StatesEnum = StatesEnum.MOVE_TOWARD_T
     # DEFINE STATES HERE!
-
+    
     while robot.step(TIME_STEP) != -1:
 
         gps_values,compass_val,sonar_value,position_value,ir_value = read_sensors_values()
         ir_value_1, ir_value_6, ir_value_3, ir_value_5, ir_value_2, ir_value_4 = ir_value
-       
+        x_current, y_current, _ = gps_values
 
         robot_position = update_robot_state()
-        if ir_value_1 < blind_spot_ir: 
-            move_robot(0, -10, 0, robot_position[2])
-        else : 
-            move_robot(-10, 0, 0, robot_position[2])
+        # theta = get_bearing_in_degrees(compass_val)
+        theta = math.atan2(Y_GOAL - gps_values[1], X_GOAL - gps_values[0]) * 180 / math.pi
+        print(theta)
+        if state == StatesEnum.MOVE_TOWARD_T:
+            if ir_value_1 < blind_spot_ir: # if it detects an obstacle  
+                state = StatesEnum.FOLLOW_BOUNDARY
+            else:
+                sin_theta = math.sin(theta)
+                cos_theta = math.cos(theta)
+                move_robot(10 * cos_theta, 10 * sin_theta, 0, robot_position[2])
+        elif state == StatesEnum.FOLLOW_BOUNDARY:
+            move_robot(0, 0, 0, robot_position[2])
+        elif state == StatesEnum.STOP:
+            ...
+
+        # if ir_value_1 < blind_spot_ir: 
+        #     move_robot(0, -10, 0, robot_position[2])
+        # else : 
+        #     move_robot(-10, 0, 0, robot_position[2])
         # update_robot_state(ir_value_1) 
         # move_robot('right') 
         # DEFINE STATE MACHINE HERE!
